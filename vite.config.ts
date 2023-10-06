2a23e737eab9b8c886365d248f8f395d537e2e76
elliott-king <8884571+elliott-king@users.noreply.github.com>
2023-10-06 15:08:11 -0400

    fix: Fix weird import from vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,36 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import istanbul from "vite-plugin-istanbul";
 
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import url from 'node:url'
+import { createRequire } from 'node:module'
+
+const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`
+function reactVirtualized() {
+  return {
+    name: 'flat:react-virtualized',
+    // Note: we cannot use the `transform` hook here
+    //       because libraries are pre-bundled in vite directly,
+    //       plugins aren't able to hack that step currently.
+    //       so instead we manually edit the file in node_modules.
+    //       all we need is to find the timing before pre-bundling.
+    configResolved: async () => {
+      const require = createRequire(import.meta.url)
+      const reactVirtualizedPath = require.resolve('react-virtualized')
+      const { pathname: reactVirtualizedFilePath } = new url.URL(reactVirtualizedPath, import.meta.url)
+      const file = reactVirtualizedFilePath
+        .replace(
+          path.join('dist', 'commonjs', 'index.js'),
+          path.join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js'),
+        )
+      const code = await fs.readFile(file, 'utf-8')
+      const modified = code.replace(WRONG_CODE, '')
+      await fs.writeFile(file, modified)
+    },
+  }
+}
+
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE");
   return {
@@ -25,6 +55,9 @@ export default defineConfig(({ command, mode }) => {
         exclude: ["node_modules", "cypress", "dist"],
         forceBuildInstrument: true,
       }),
+      // HACK to fix weirdly-included import
+      // https://github.com/bvaughn/react-virtualized/issues/1632
+      reactVirtualized(),
     ],
     // to get aws amplify to work with vite
     resolve: {
