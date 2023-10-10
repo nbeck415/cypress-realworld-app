a4da3503e3f968266a7afab58851f23acec24a9e
Natalie <natalielunbeck@yahoo.com>
2023-10-10 13:56:08 -0700

    clean up tests

--- cypress/tests/shipyard/manage-user.spec.ts
+++ cypress/tests/shipyard/manage-user.spec.ts
@@ -27,12 +27,6 @@ describe("Create new user", function () {
     // add user to db
     cy.get("button[type='submit']").click();
 
-    // want to change this to check for most recent entry, but stuck
-    // adds the user to the middle of the db
-    // maybe we sort db by date-created field
-    cy.database("find", "users", { username: username }).then((user: User) => {
-      expect(user.username).to.equal(username);
-    });
   });
 });
 
@@ -45,59 +39,3 @@ describe("Login as new user", function () {
     cy.get("button[type='submit']").click();
   });
 });
-
-/*
-describe("Edit existing user", function () {
-    const testEmail = "Norene39@yahoo.com"
-    beforeEach(function () {
-      const urlToVisit = "/";
-      cy.visit(urlToVisit);
-      //cy.task("db:seed");
-      /*
-      cy.intercept("PATCH", "/users/*").as("updateUser");
-      cy.intercept("GET", "/notifications*").as("getNotifications");
-      
-      cy.database("find", "users", { email: testEmail }).then((user) => {
-        if (user) {
-          cy.loginByXstate(user.username);
-        }
-      });
-      
-      cy.getBySel("sidenav-user-settings").click();
-
-    });
-    it("should edit an existing user", function () {
-      cy.visit('/user/settings')
-      // tracking using a random ID
-      const userId = Cypress._.random(1000, 9999);
-      const newName = `Name${userId}`
-      const newLast = `T${userId}`
-      const newEmail = `${newName}@gmail.com`
-      const newNumber = "415-555-8992"
-      // clear fields
-      cy.get("input[name='firstName']").clear();
-      cy.get("input[name='lastName']").clear();
-      cy.get("input[name='email']").clear();
-      cy.get("input[name='phoneNumber']").clear();  
-      // type inputs
-      cy.get("input[name='firstName']").type(newName);
-      cy.get("input[name='lastName']").type(newLast);
-      cy.get("input[name='email']").type(newEmail);
-      cy.get("input[name='phoneNumber']").type(newNumber);  
-      // edit user in db
-      cy.get("button[type='submit']").click();
-  
-      cy.database("find", "users", { email: newEmail })
-      .then((user: User) => {
-        if (user) {
-          cy.log(`Found user with email: ${user.email}`);
-          expect(user.email).to.equal(newEmail);
-          expect(user.firstName).to.equal(newName);
-          expect(user.lastName).to.equal(newLast);
-          expect(user.phoneNumber).to.equal(newNumber);
-        } else {
-          cy.log(`User with email "${testEmail}" not found in the database.`);
-        }
-      })
-    });
-  });
