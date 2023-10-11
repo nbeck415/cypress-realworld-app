c9e1f8b07dab0decb6e136b537046af8f0f11936
Natalie <natalielunbeck@yahoo.com>
2023-10-11 16:58:51 -0700

    clean up sample E2E tests

--- cypress/tests/shipyard/manage-user.spec.ts
+++ cypress/tests/shipyard/manage-user.spec.ts
@@ -1,80 +1,57 @@
 import { User } from "models";
 const userId = Cypress._.random(1000, 9999);
+
+before(function() {
+  const urlToVisit = "/signup";
+  cy.visit(urlToVisit);
+});
+
 describe("Create new user", function () {
-  beforeEach(function () {
-    //console.log(process.env.SHIPYARD_DOMAIN_FRONTEND)
-    const urlToVisit = "/signup";
-    cy.visit(urlToVisit);
-    cy.url().then((url) => {
-      cy.log(`Current URL: ${url}`);
-    });
-    //cy.task("db:seed");
-  });
   // go to sign up page and register
   it("should create a new user", function () {
-    //cy.visit('/signup')
     const username = `user${userId}`;
     const firstName = `Name${userId}`
     const lastName = `T${userId}`
     cy.get("input[name='username']").type(username);
     cy.get("input[name='firstName']").type(firstName);
     cy.get("input[name='lastName']").type(lastName);
-    // only field that really matters is username because uses ID
-    // maybe we track user by the auto assigned ID instead of username?
     cy.get("input[name='password']").type("testingPwd");
     cy.get("input[name='confirmPassword']").type("testingPwd");
 
     // add user to db
     cy.get("button[type='submit']").click();
-
+    cy.wait(2000);
   });
-});
-
-describe("Login as new user", function () {
   it("should log in as the user we just created", function () {
     cy.visit('/');
+    // use existing creds
     const username = `user${userId}`;
     const password = "testingPwd";
     cy.get("input[name='username']").type(username);
     cy.get("input[name='password']").type(password);
+    // click to log in
     cy.get("button[type='submit']").click();
-  });
-});
-
-describe("Initialize user", function() {
-  beforeEach(function () {
-    cy.visit('/signup');
-    const username = `user${userId}`;
-    const firstName = `Name${userId}`
-    const lastName = `T${userId}`
-    cy.get("input[name='username']").type(username);
-    cy.get("input[name='firstName']").type(firstName);
-    cy.get("input[name='lastName']").type(lastName);
-    // only field that really matters is username because uses ID
-    // maybe we track user by the auto assigned ID instead of username?
-    cy.get("input[name='password']").type("testingPwd");
-    cy.get("input[name='confirmPassword']").type("testingPwd");
-
-    // add user to db
-    cy.get("button[type='submit']").click();
-
-    cy.visit('/');
-    const password = "testingPwd";
-    cy.get("input[name='username']").type(username);
-    cy.get("input[name='password']").type(password);
-    cy.get("button[type='submit']").click();
-  });
-  it("should register user for a bank account", function() {
+    cy.wait(4000);
+    // "onboarding" flow
     cy.contains('button', 'Next').click();
+    cy.wait(2000);
+    // enter bank info
     cy.get("input[name='bankName']").type("US Bank");
     cy.get("input[name='routingNumber']").type("987123745");
     cy.get("input[name='accountNumber']").type("112987234");
     cy.get("button[type='submit']").click();
+    cy.wait(2000);
+    // click "done"
     cy.contains('button', 'Done').click();
-
-  });
-  it("should edit a user's information", function () {
+    cy.wait(2000);
+    // go to edit user page
     cy.getBySel("sidenav-user-settings").click();
-    
+    cy.wait(2000);
+    cy.get("input[name='email']").type("email@gmail.com");
+    cy.get("input[name='phoneNumber']").type("4155558912");
+    cy.wait(2000);
+    // make sure this button is of type "submit"
+    cy.contains('Save').should('have.attr', 'type', 'submit');
+    cy.wait(2000);
   });
 });
